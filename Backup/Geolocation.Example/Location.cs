cf348f1841892509dc5cd01562aafb5fbee3a5d0
Scott Schluer <scott.schluer@gmail.com>
2012-12-12 19:16:30 -0800

    Upgrade to Visual Studio 2012 solution

--- Backup/Geolocation.Example/Location.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Geolocation.Example
-{
-    public class Location
-    {
-        public string Name { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-    }
-}
--- Backup/Geolocation.Example/Result.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Geolocation.Example
-{
-    public class Result
-    {
-        public string Name { get; set; }
-        public double Distance { get; set; }
-        public string Direction { get; set; }
-    }
-}
