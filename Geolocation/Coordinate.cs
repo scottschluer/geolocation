0a7ce905b70a349c8a202adf6f1e4c1927061b85
Shinji Yoshida <yoshida@nurve.jp>
2017-07-07 10:43:11 +0900

    Add ctor to Coordinate in order to make sample code work

--- Geolocation/Coordinate.cs
+++ Geolocation/Coordinate.cs
@@ -10,5 +10,10 @@ namespace Geolocation
     {
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+
+        public Coordinate(double latitude, double longitude) {
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+        }
     }
 }
